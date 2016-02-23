5caa2d43d21a7b7ea11eef6c7d54df0d41cbf3c5
Sun Kewei <skw@Kewei-Sun.local>
2016-02-23 13:42:46 +0800

    publish test

--- src/main/java/jenkins/HelloWorld.java
+++ src/main/java/jenkins/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
         System.out.println("Hello world!");
         System.out.println("jenkins");
         System.out.println("git publisher");
+        System.out.println("publish test");
     }
 
 }
